f6c2bfef8498aeea0308398c52ac867158bdd640
Diveeyha <67159522+Diveeyha@users.noreply.github.com>
2024-04-08 03:41:09 -0400

    Update eBird_weather.py

--- eBird_weather.py
+++ eBird_weather.py
@@ -181,7 +181,7 @@ def main():
             st.stop()
             # st.write(time_zone)
 
-    time_col1, time_col2 = st.columns([5, 1])
+    time_col1, time_col2 = st.columns([1, 5])
     time_col1.radio("Time", ["Current", "Other Time"], horizontal=True, label_visibility="collapsed",
               key="radio_time")
     utc_offset = 24 - (datetime.now(zoneinfo.ZoneInfo(time_zone)).utcoffset().seconds/3600)
